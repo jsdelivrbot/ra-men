2d03d43c657457240a7f6f9fda56a0812ad6a597
hydraslay <xvkai2010@gmail.com>
2018-02-20 12:25:57 +0900

    try env

--- webpack/node.production.js
+++ webpack/node.production.js
@@ -23,7 +23,7 @@ module.exports = merge(common, {
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': JSON.stringify('production'),
-                'PORT': '"process.env.PORT"'
+                'PORT': '"' + process.env.PORT + '"'
             }
         })
     ]
