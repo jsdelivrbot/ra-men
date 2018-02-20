a10e96c8be91e0e53c7af361613541929ec7c663
hydraslay <xvkai2010@gmail.com>
2018-02-20 18:28:31 +0900

    commit with error

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { hydrate } from 'react-dom';
-
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import App from '../shared/App';
+
+const preloadedState = window.__PRELOADED_STATE__
+delete window.__PRELOADED_STATE__
+const store = createStore(App, preloadedState)
 
 /**
  * Renders a react component into the #react-root div container.
@@ -10,14 +15,16 @@ import App from '../shared/App';
  *
  * @param Component React component that should be rendered
  */
-const render = Component => {
+const render = (Component, store) => {
     hydrate(
-        <Component/>,
+        <Provider store={store}>
+            <Component />
+        </Provider>,
         document.getElementById('react-root')
     );
 };
 
-render(App);
+render(App, store);
 
 /**
  * This script provides hot module reloading in development mode.
@@ -25,6 +32,6 @@ render(App);
 if (module.hot && process.env.NODE_ENV === 'development') {
     module.hot.accept('../shared/App', () => {
         const App = require('../shared/App').default;
-        render(App);
+        render(App, store);
     });
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,11 @@
+import qs from 'qs';
 import React from 'react';
 import ReactDOM from 'react-dom/server';
 import { flushChunkNames } from 'react-universal-component/server';
 import flushChunks from 'webpack-flush-chunks';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { fetchCounter } from '../shared/counter'
 
 import App from '../shared/App';
 
@@ -14,18 +18,56 @@ import App from '../shared/App';
  * @param clientStats Parameter passed by hot server middleware
  */
 export default ({ clientStats }) => async (req, res) => {
-    const app = (
-        <App/>
-    );
 
-    const appString = ReactDOM.renderToString(app);
-    const chunkNames = flushChunkNames();
-    const { js, styles, cssHash } = flushChunks(clientStats, { chunkNames });
+    var renderFullPage = function (html, preloadedState) {
+        return `
+          <!doctype html>
+          <html>
+            <head>
+              <title>Redux Universal Example</title>
+            </head>
+            <body>
+              <div id="root">${html}</div>
+              <script>
+                // WARNING: See the following for security issues around embedding JSON in HTML:
+                // http://redux.js.org/docs/recipes/ServerRendering.html#security-considerations
+                window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
+              </script>
+              <script src="/static/bundle.js"></script>
+            </body>
+          </html>
+          `
+      }
 
-    res.render('index', {
-        appString,
-        js,
-        styles,
-        cssHash
+    fetchCounter(apiResult => {
+        const params = qs.parse(req.query)
+        const counter = parseInt(params.counter, 10) || 0
+        // Compile an initial state
+        let preloadedState = { counter }
+     
+        // Create a new Redux store instance
+        const store = createStore(App, preloadedState)
+     
+        const appString = ReactDOM.renderToString(
+            <Provider store={store}>
+                <App />
+            </Provider>
+        );
+
+        // Grab the initial state from our Redux store
+        const finalState = store.getState()
+         
+        // Send the rendered page back to the client
+        res.send(renderFullPage(html, finalState))
+
+        // const chunkNames = flushChunkNames();
+        // const { js, styles, cssHash } = flushChunks(clientStats, { chunkNames });
+
+        // res.render('index', {
+        //     appString,
+        //     js,
+        //     styles,
+        //     cssHash
+        // });
     });
 };
